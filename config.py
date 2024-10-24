49fbea2aef7e9e6fc3a08d7c5c04a70dc57f0d43
nxivm-bots <massacres@myyahoo.com>
2024-10-24 15:48:33 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,12 +5,12 @@ from logging.handlers import RotatingFileHandler
 
 
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "7356883802:AAHeSRtPXQtZDHFRUY7vtKOywI-lZz9i69M")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "7798258716:AAHC5FwRGZiN58wtzgmAF14sBNyM0cICWQU")
 API_ID = int(os.environ.get("API_ID", "29872536"))
 API_HASH = os.environ.get("API_HASH", "65e1f714a47c0879734553dc460e98d6")
 
 
-OWNER_ID = int(os.environ.get("OWNER_ID", "6698364560"))
+OWNER_ID = int(os.environ.get("OWNER_ID", "1110013191"))
 DB_URL = os.environ.get("DB_URL", "mongodb+srv://denji3494:denji3494@cluster0.bskf1po.mongodb.net/")
 DB_NAME = os.environ.get("DB_NAME", "madflixbotz")
 JOIN_REQ_DB = os.environ.get("JOIN_REQ_DB", DB_URL)
@@ -23,13 +23,13 @@ FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1002023569912"))
 FILE_AUTO_DELETE = int(os.getenv("FILE_AUTO_DELETE", "1800")) # auto delete in seconds
 
 
-PORT = os.environ.get("PORT", "8040")
+PORT = os.environ.get("PORT", "8080")
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
 
 
 try:
-    ADMINS=[1110013191]
+    ADMINS=[6698364560]
     for x in (os.environ.get("ADMINS", "1110013191").split()):
         ADMINS.append(int(x))
 except ValueError:
